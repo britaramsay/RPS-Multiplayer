1f23807028fadb920d5b16f124a1c53e88538fa7
Brita Ramsay <britaramsay@gmail.com>
2018-03-23 18:02:21 -0400

    Each players name shows on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,9 @@ var database = firebase.database();
 var player1Choice,
     player2Choice,
     player1Wins,
-    player2Wins;
+    player2Wins,
+    numUsers,
+    numPlayers;
 
 // Add ourselves to presence list when online.
 var connectionsRef = database.ref('/connections');
@@ -25,16 +27,50 @@ var connectedRef = database.ref('.info/connected');
 connectedRef.on('value', function(snapshot) {
   if(snapshot.val()) {
     var con = connectionsRef.push(true);
-    console.log(con);
     con.onDisconnect().remove();
   }
 });
 
 // When first loaded or when the connections list changes...
 connectionsRef.on("value", function(snap) {
-    var numUsers = snap.numChildren();
-    // Display the viewer count in the html.
+    
+    numUsers = snap.numChildren();
     // The number of online users is the number of children in the connections list.
-    if(numUsers > 1)    $('#watchers').text(numUsers + ' users online');
-    else    $('#watchers').text(numUsers + ' user online');
+    if(numUsers > 1) {
+        $('#watchers').text(numUsers + ' users online');
+    }
+    else {
+        $('#watchers').text(numUsers + ' user online');
+    }
 });
+
+$('#submit-name').on('click', function() {
+    event.preventDefault();
+
+    var playerName = $('#player-name').val().trim();
+
+    if(numPlayers < 3) {
+        console.log(numPlayers);
+
+        database.ref('/players/' + numPlayers).set({
+            name: playerName,
+            wins: 0,
+            losses: 0
+        });
+    }
+});
+
+// When a new entry is added to players in firebase
+database.ref("/players").on("value", function(snapshot) {
+    console.log(snapshot.val());
+    // If one person has already entered their name
+    if (!snapshot.child(1).exists()){
+        // Set numPlayers to 2
+        numPlayers = 1;
+    }
+    else {
+        $('#player-'+numPlayers).html(snapshot.child(numPlayers+'/name').val());
+
+        numPlayers++;
+    }
+})
